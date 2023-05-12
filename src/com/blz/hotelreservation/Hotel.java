e65f42bd8894f47cb60f207d522b37ce1c1a20c4
Kapilm2599 <mahajankapil575@gmail.com>
2023-05-12 18:12:40 +0530

    Added: Hotel Reservation Program

--- src/com/blz/hotelreservation/Hotel.java
+++ src/com/blz/hotelreservation/Hotel.java
@@ -1,75 +1,82 @@
 package com.blz.hotelreservation;
 
 public class Hotel {
+
+		public String hotelName;
+	    public int weekdayRateForRegular;
+	    public int weekendRateForRegular;
+	    public int weekdayRateForRewarder;
+	    public int weekendRateForRewarder;
+	    public int hotelRatings;
+
+	    public Hotel(String hotelName, int weekdayRateForRegular, int weekendRateForRegular, int weekdayRateForRewarder, int weekendRateForRewarder, int hotelRatings) {
+	        this.hotelName = hotelName;
+	        this.weekdayRateForRegular = weekdayRateForRegular;
+	        this.weekendRateForRegular = weekendRateForRegular;
+	        this.weekdayRateForRewarder = weekdayRateForRewarder;
+	        this.weekendRateForRewarder= weekendRateForRewarder;
+	        this.hotelRatings = hotelRatings;
+	    }
+
+	    public void setHotelName(String hotelName) {
+	        this.hotelName = hotelName;
+	    }
+
+	    public String getHotelName() {
+	        return hotelName;
+	    }
+
+	    public int getWeekdayRateForRegularCustomer() {
+	        return weekdayRateForRegular;
+	    }
+
+	    public void setWeekdayRateForRegularCustomer(int weekdayRate) {
+	        this.weekdayRateForRegular = weekdayRate;
+	    }
+
+	    public int getWeekendRateForRegularCustomer() {
+	        return weekendRateForRegular;
+	    }
+
+	    public void setWeekendRateForRegularCustomer(int weekendRate) {
+	        this.weekendRateForRegular = weekendRate;
+	    }
+
+
+	    public int getWeekdayRateForRewarder() {
+	        return weekdayRateForRewarder;
+	    }
+
+	    public void setWeekdayRateForRewarder(int weekdayRateForRewarder) {
+	        this.weekdayRateForRewarder = weekdayRateForRewarder;
+	    }
+
+	    public int getWeekendRateForRewarder() {
+	        return weekendRateForRewarder;
+	    }
+
+	    public void setWeekendRateForRewarder(int weekendRateForRewarder) {
+	        this.weekendRateForRewarder = weekendRateForRewarder;
+	    }
+
+	    public int getHotelRatings() {
+	        return hotelRatings;
+	    }
+
+	    public void setHotelRatings(int hotelRatings) {
+	        this.hotelRatings = hotelRatings;
+	    }
+
+	    @Override
+	    public String toString() {
+	        return "\n" + "Hotel{" +
+	                "HotelName='" + hotelName + '\'' +
+	                ", WeekdayRateForRegular=" + weekdayRateForRegular + '\'' +
+	                ",WeekendRateForRewarder+" + weekendRateForRegular +
+	                ", WeekdayRateForRegular=" + weekdayRateForRewarder + '\'' +
+	                ",WeekendRateForRewarder+" + weekendRateForRewarder +
+	                ",hotelRatings+" + hotelRatings +
+	                '}';
+	    }
 	
-	public String hotelName;
-	public int weekdayRateForRegular;
-	public int weekendRateForRegular;
-	public int weekdayRateForReward;
-	public int weekendRateForReward;
-	public int hotelRatings;
-	
-	public Hotel(String hotelName, int weekdayRateForRegular, int weekendRateForRegular, int weekdayRateForReward,
-			int weekendRateForReward, int hotelRatings) {
-		
-		super();
-		this.hotelName = hotelName;
-		this.weekdayRateForRegular = weekdayRateForRegular;
-		this.weekendRateForRegular = weekendRateForRegular;
-		this.weekdayRateForReward = weekdayRateForReward;
-		this.weekendRateForReward = weekendRateForReward;
-		this.hotelRatings = hotelRatings;
-		
-		}
-					Public String getHotelName() {
-						return hotelName;
-					}
-					public void setHotelName(hotelName) {
-						this.hotelName = hotelName;
-					}
-					
-					public int getWeekdayRateForRegular() {
-						return weekdayRateForRegular;
-					}
-					public void setWeekdayRateForRegular(weekdayRateForRegular) {
-						this.weekdayRateForRegular = weekdayRateForRegular;
-					}
-					public int getWeekendRateForRegular() {
-						return weekendRateForRegular;
-					}
-					public void setWeekendRateForRegular((weekendRateForRegular) {
-						this.weekendRateForRegular = weekendRateForRegular;
-					}
-					public int getweekdayRateForReward() {
-						return weekdayRateForReward;
-					}
-					public void setWeekdayRateForReward(weekdayRateForReward) {
-						this.weekdayRateForReward = weekdayRateForReward;
-					}
-					public int getWeekendRateForReward() {
-						return weekendRateForReward;
-					}
-					public void setWeekendRateForReward(weekendRateForReward) {
-						this.weekendRateForReward = weekendRateForReward;
-					}
-					public int getHotelRatings() {
-						return hotelRatings;
-					}
-					public void setHotelRatings(hotelRatings) {
-						this.hotelRatings = hotelRatings;
-					}	
-					
-					
-						
-						
-					}
-		
-		
-	}
-
-
-
-
-	
-
-}
+}	
